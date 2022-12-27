f29dadf3abe5e7158df08f79c0b6883f88ebcbc1
Taehyun Kim <thkim0022@icloud.com>
2022-12-27 18:38:35 +0900

    Update terraform/lambda/main.tf

--- terraform/lambda/main.tf
+++ terraform/lambda/main.tf
@@ -1,7 +1,8 @@
 module "lambda" {
-  source = "terraform-aws-lambda"
+  source  = "app.terraform.io/thkim0022/lambda/aws"
+  version = "1.0.0"
 
-  lambda_function_name = var.lambda_function_name
+  function_name = var.lambda_function_name
   s3_bucket_name       = var.s3_bucket_name
 
   tags = var.tags
