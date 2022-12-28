9e33cdfbe9053ea3b68ed31661ec4193e52afd4b
Taehyun Kim <thkim0022@icloud.com>
2022-12-28 17:42:19 +0900

    Create S3 Bucket Notification for triggering

--- terraform/_module/s3/s3.tf
+++ terraform/_module/s3/s3.tf
@@ -9,3 +9,12 @@ resource "aws_s3_bucket" "this" {
     }
   )
 }
+
+resource "aws_s3_bucket_notification" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  lambda_function {
+    lambda_function_arn = var.lambda_function_arn
+    events = ["s3:ObjectCreated:*"]
+  }
+}
--- terraform/terraform.auto.tfvars
+++ terraform/terraform.auto.tfvars
@@ -5,6 +5,7 @@ region = "ap-northeast-2"
 function_name = "excel-to-mysql"
 handler = "lambda_function.lambda_handler"
 runtime = "python3.9"
+timeout = "60"
 
 # Tags
 tags = {
