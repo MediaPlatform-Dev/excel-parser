3d2b4b984c5eea0980e9d669ea6fce424d4acc4c
Taehyun Kim <thkim0022@icloud.com>
2022-12-28 17:03:25 +0900

    Split IAM module

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,10 +6,19 @@ module "s3" {
   tags = var.tags
 }
 
+module "iam" {
+  source = "./_module/iam"
+
+  function_name = var.function_name
+
+  tags = var.tags
+}
+
 module "lambda" {
   source  = "./_module/lambda"
 
   function_name  = var.function_name
+  iam_role_arn = module.iam.arn
   handler = var.handler
   runtime = var.runtime
 
