8b2cea837f94e3ad8d8a093abdb73c39697a8512
Taehyun Kim <thkim0022@icloud.com>
2022-12-29 14:49:00 +0900

    Update python/lambda_function.py

--- python/lambda_function.py
+++ python/lambda_function.py
@@ -13,7 +13,7 @@ class Excel:
         self.file_name = event['Records'][0]['s3']['object']['key']
 
     def get_s3_object(self):
-        return boto3.client('s3').get_object(Bucket=self.bucket_name, Key=self.file_name)['Body'].read()
+        return boto3.client('s3').get_object(Bucket=self.bucket_name, Key=self.file_name)['Body'].read().decode('utf-8')
 
 
 def lambda_handler(event, _context):
