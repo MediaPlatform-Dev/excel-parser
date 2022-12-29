0009bdf663d96300ead085fedc77f08c5e34cafb
Taehyun Kim <thkim0022@icloud.com>
2022-12-29 15:27:51 +0900

    Update python/lambda_function.py

--- python/lambda_function.py
+++ python/lambda_function.py
@@ -12,10 +12,12 @@ def lambda_handler(event, _context):
     file_name = event['Records'][0]['s3']['object']['key']
 
     obj = boto3.client('s3').get_object(Bucket=bucket_name, Key=file_name)
+    print(obj)
 
-    if obj['ContentType'] == 'text/csv':
-        df = pd.read_csv(obj['Body'])
-    else:
-        df = pd.read_excel(obj['Body'])
+    # if obj['ContentType'] == 'text/csv':
+    #     df = pd.read_csv(obj['Body'])
+    # else:
+    #     df = pd.read_excel(obj['Body'])
 
+    df = pd.read_csv(obj['Body'])
     print(df.iloc[:5, :])
